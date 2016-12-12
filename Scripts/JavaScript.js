05f32525959e3c864fc50f65fb662b9000191c64
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-12-12 11:53:12 +0100

    No actualiza del todo bien

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,36 @@
+﻿function saludo() {
+    var texto = "Hola Mundo";
+    document.getElementById("divMensaje").innerHTML = texto;
+}
+
+function saludo2() {
+    var nombre=document.getElementById("txtNombre").value;
+    var apellidos=document.getElementById("txtApellidos").value;
+    if(nombre.length!=0 && apellidos.length!=0){
+    var texto = "Hola "+nombre+" "+apellidos;
+    document.getElementById("divMensaje").innerHTML = texto;
+    } else {
+        if (nombre.length == 0) {
+            document.getElementById("errorNombre").innerHTML = "Error, el nombre está vacío";
+        }
+        if (apellidos.length == 0) {
+            document.getElementById("errorApellidos").innerHTML = "Error, el apellido está vacío";
+        }
+    }
+}
+
+function puedeSaludar() {
+    var nombre = document.getElementById("txtNombre").value;
+    var apellidos = document.getElementById("txtApellidos").value;
+    if (nombre.length != 0 && apellidos.length != 0) {
+        document.getElementById("boton2").removeAttribute("disabled");
+    } else {
+        document.getElementById("boton2").setAttribute("disabled", "disabled");
+    }
+}
+function saluda() {
+    var nombre = document.getElementById("txtNombre").value;
+    var apellidos = document.getElementById("txtApellidos").value;
+    var texto = "Hola " + nombre + " " + apellidos;
+    document.getElementById("divMensaje").innerHTML = texto;
+}
